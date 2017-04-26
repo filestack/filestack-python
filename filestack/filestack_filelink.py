693be6146174047f6ce507bc1027dab4e2ff0d55
Wyatt Barnes <wyatt@filestack.com>
2017-04-26 11:32:09 -0500

    Add URL and handle properties

--- filestack/filestack_filelink.py
+++ filestack/filestack_filelink.py
@@ -3,15 +3,25 @@ from filestack.version import __version__
 
 class Filelink(object):
 
+    FILESTACK_CDN_URL = 'https://cdn.filestackcontent.com/'
     HEADERS = {'User-Agent': 'filestack-python {}'.format(__version__)}
 
-    def __init__(self, apikey=None):
+    def __init__(self, handle, apikey=None):
         self._apikey = apikey
+        self._handle = handle
 
-    def get_apikey(self):
+    @property
+    def handle(self):
+        return self._handle
+
+    @property
+    def url(self):
+        return self.FILESTACK_CDN_URL + self._handle
+
+    @property
+    def apikey(self):
         return self._apikey
 
-    def set_apikey(self, apikey):
+    @apikey.setter
+    def apikey(self, apikey):
         self._apikey = apikey
-
-    apikey = property(get_apikey, set_apikey)
