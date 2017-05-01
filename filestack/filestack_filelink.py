85fe86f47a013c907a3e3ba8b79ccba20d1f7808
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-01 12:46:48 -0700

    add URL link from config

--- filestack/filestack_filelink.py
+++ filestack/filestack_filelink.py
@@ -1,8 +1,11 @@
+from filestack.config import FILESTACK_CDN_URL
+
 class Filelink(object):
 
     def __init__(self, handle, apikey=None):
         self._apikey = apikey
         self._handle = handle
+        self.FILESTACK_CDN_URL = FILESTACK_CDN_URL
 
     @property
     def handle(self):
