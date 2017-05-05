642bcf7b0e981a1ba4091ae326e298b318a95675
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-05 14:02:55 -0700

    change PolicyException to SecurityException

--- filestack/filestack_security.py
+++ filestack/filestack_security.py
@@ -3,17 +3,17 @@ import base64
 import hmac
 import hashlib
 from filestack.config import ACCEPTED_SECURITY_TYPES
-from filestack.exceptions import PolicyError
+from filestack.exceptions import SecurityError
 
 
 def validate(policy):
     for param, value in policy.items():
 
         if param not in ACCEPTED_SECURITY_TYPES.keys():
-            raise PolicyError('Invalid Security Parameter: {}'.format(param))
+            raise SecurityError('Invalid Security Parameter: {}'.format(param))
 
         if type(value) != ACCEPTED_SECURITY_TYPES[param]:
-            raise PolicyError('Invalid Parameter Data Type for {}, '
+            raise SecurityError('Invalid Parameter Data Type for {}, '
                               'Expecting: {} Received: {}'.format(
                                 param, ACCEPTED_SECURITY_TYPES[param],
                                 type(value)))
