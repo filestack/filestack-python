5228463d76da33c84f7887070cca3a03c55d7460
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-05 14:03:32 -0700

    add trafarets

--- filestack/trafarets.py
+++ filestack/trafarets.py
@@ -1,5 +1,6 @@
 import trafaret as t
 
+import json
 POLICY_SCHEMA = t.Dict({
     'call': t.List(t.Enum('call', 'handle', 'url', 'maxSize', 'minSize', 'path', 'container')),
     'handle': t.String(),
@@ -10,15 +11,21 @@ POLICY_SCHEMA = t.Dict({
     'container': t.String()
 })
 
+POLICY_SCHEMA.make_optional('*')
+
 CONTENT_DOWNLOAD_SCHEMA = t.Dict({
     'dl': t.Bool(),
     'cache': t.Bool()
 })
 
+CONTENT_DOWNLOAD_SCHEMA.make_optional('*')
+
 OVERWRITE_SCHEMA = t.Dict({
     'url': t.String(),
     'base64decode': t.Bool()
 })
+
+OVERWRITE_SCHEMA.make_optional('*')
 
 METADATA_SCHEMA = t.Dict({
     'size': t.Bool(),
