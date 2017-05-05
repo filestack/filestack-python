8ff00b769b877621e1bd393152cd6e19dccaadb0
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-05 14:01:06 -0700

    add trafaret typechecks, fix spacing;

--- filestack/filestack_common.py
+++ filestack/filestack_common.py
@@ -3,16 +3,20 @@ import os
 import mimetypes
 
 from filestack.config import API_URL, FILE_PATH, HEADERS
+from filestack.exceptions import SecurityError
+from filestack.trafarets import CONTENT_DOWNLOAD_SCHEMA, OVERWRITE_SCHEMA, METADATA_SCHEMA
 
 class CommonMixin(object):
 
     def download(self, destination_path, params=None, security=None):
+        if params:
+            CONTENT_DOWNLOAD_SCHEMA.check(params)
         with open(destination_path, 'wb') as f:
             response = self._make_call(API_URL, 'get',
-                                        path=FILE_PATH,
-                                        handle=self.handle,
-                                        params=params,
-                                        security_required=self.security)
+                                       path=FILE_PATH,
+                                       handle=self.handle,
+                                       params=params,
+                                       security_required=self.security)
 
             if response.ok:
                 for chunk in response.iter_content(1024):
@@ -22,40 +26,59 @@ class CommonMixin(object):
             return response
 
     def get_content(self, params=None, security=None):
+        if params:
+            CONTENT_DOWNLOAD_SCHEMA.check(params)
         response = self._make_call(API_URL, 'get',
-                                    path=FILE_PATH,
-                                    handle=self.handle,
-                                    params=params,
-                                    security_required=self.security)
+                                   path=FILE_PATH,
+                                   handle=self.handle,
+                                   params=params,
+                                   security_required=self.security)
 
         return response.content
 
     def delete(self, params=None):
-        return self._make_call(API_URL, 'delete', path=FILE_PATH, params=params, security_required=True)
+        if params:
+            params['key'] = self.apikey
+        else:
+            params = {'key': self.apikey}
+        return self._make_call(API_URL, 'delete',
+                               path=FILE_PATH,
+                               handle=self.handle,
+                               params=params,
+                               security_required=True)
 
     def overwrite(self, url=None, filepath=None, params=None):
-        data = {}
+        if params:
+            OVERWRITE_SCHEMA.check(params)
+        data, files = None, None
         if url:
-            data['url'] = url
+            data ={'url': url}
         elif filepath:
             filename = os.path.basename(filepath)
             mimetype = mimetypes.guess_type(filepath)[0]
             files = {'fileUpload': (filename, open(filepath, 'rb'), mimetype)}
         else:
-            return "You must specify a filepath or URL"
+            raise ValueError("You must include a url or filepath parameter")
 
-        return self._make_call(API_URL, 'post', path=FILE_PATH, params=params, data=data, files=files, security_required=True)
+        response = self._make_call(API_URL, 'post',
+                                   path=FILE_PATH,
+                                   params=params,
+                                   data=data,
+                                   files=files,
+                                   security_required=True)
 
-    def _make_call(self, base, action, handle=None, path=None, params=None, security_required=False, data=None, files=None):
+        new_file_info = response.json()
+        return self.__class__(handle=new_file_info.get('handle'),
+                              apikey=self.apikey,
+                              security=self.security)
+
+    def _make_call(self, base, action, handle=None, path=None, params=None, security_required=None, data=None, files=None):
         request_func = getattr(requests, action)
-
         if security_required is not None:
-
             if self.security is not None:
                 url = self._get_url(base, path=path, handle=handle, security=self.security)
-
             else:
-                return "Please provide either a signature/policy or a username/password"
+                raise SecurityError("Please provide either a signature/policy or a username/password")
 
         else:
             url = self._get_url(base, path=path, handle=handle)
@@ -63,7 +86,6 @@ class CommonMixin(object):
         return request_func(url, params=params, headers=HEADERS, data=data, files=files)
 
     def _get_url(self, base, handle=None, path=None, security=None):
-
         url_components = [base]
 
         if path:
