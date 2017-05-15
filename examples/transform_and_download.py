7c7f61787d988cd54a3c26f2ab297b989ec3ee15
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-15 08:42:16 -0700

    add transformation example

---
+++ examples/transform_and_download.py
@@ -0,0 +1,8 @@
+from filestack import Client
+
+client = Client('<YOUR_API_KEY>')
+transform = client.transform_external('https://upload.wikimedia.org/wikipedia/commons/3/32/House_sparrow04.jpg')
+transform.resize(width=500, height=500).enhance()
+
+print(transform.get_transformation_url())
+print(transform.download('bird.jpg'))
