25887fadae7a612ecb70903b0bbcb2b79505f9bb
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-23 09:35:56 -0700

    add multipart tests

---
+++ tests/multipart_test.py
@@ -0,0 +1,37 @@
+import json
+import pytest
+import requests
+import responses
+
+from filestack import Client, Filelink
+from filestack.config import MULTIPART_START_URL, MULTIPART_UPLOAD_URL, MULTIPART_COMPLETE_URL
+from filestack.utils import upload_utils
+
+APIKEY = 'APIKEY'
+HANDLE = 'SOMEHANDLE'
+URL = "https://cdn.filestackcontent.com/{}".format(HANDLE)
+
+
+@pytest.fixture
+def client():
+    return Client(APIKEY)
+
+
+@responses.activate
+def test_upload_multipart(client):
+        responses.add(responses.POST, MULTIPART_START_URL, status=200, content_type="application/json",
+                      json={"region": "us-east-1", "upload_id": "someuuid", "uri": "someuri", "location_url": "somelocation"})
+
+        responses.add(responses.POST, MULTIPART_UPLOAD_URL, status=200, content_type="application/json", json={'url': URL, "headers": {}})
+
+        def chunk_put_callback(request):
+            body = {'url': URL}
+            return (200, {'ETag': 'someetags'}, json.dumps(body))
+
+        responses.add_callback(responses.PUT, URL, callback=chunk_put_callback)
+
+        responses.add(responses.POST, MULTIPART_COMPLETE_URL, status=200, content_type="application/json", json={"url": URL})
+        new_filelink = client.upload(filepath='bird.jpg')
+
+        assert new_filelink.handle == HANDLE
+
