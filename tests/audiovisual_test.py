4043f189210dfb8a5b383fc5e69a622f3d3bd6fe
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-26 13:39:49 -0700

    add tests for av object

---
+++ tests/audiovisual_test.py
@@ -0,0 +1,38 @@
+import filestack.models
+import pytest
+
+from filestack import AudioVisual, Filelink
+from httmock import urlmatch, HTTMock, response
+
+
+APIKEY = 'APIKEY'
+HANDLE = 'SOMEHANDLE'
+URL = 'https://cdn.filestackcontent.com/{}'.format(HANDLE)
+PROCESS_URL = 'https://process.filestackapi.com/{}'.format(HANDLE)
+
+
+@pytest.fixture
+def av():
+    return AudioVisual(PROCESS_URL, apikey=APIKEY)
+
+
+def test_status(av):
+
+    @urlmatch(netloc=r'process.filestackapi\.com', method='get', scheme='https')
+    def api_zip(url, request):
+        return response(200, {'url': PROCESS_URL, 'status': 'completed'}, {'content-type': 'application/json'})
+
+    with HTTMock(api_zip):
+        assert av.status == 'completed'
+
+
+def test_convert(av):
+
+    @urlmatch(netloc=r'process.filestackapi\.com', method='get', scheme='https')
+    def api_zip(url, request):
+        return response(200, {'status': 'completed', 'data': {'url': URL}}, {'content-type': 'application/json'})
+
+    with HTTMock(api_zip):
+        filelink = av.to_filelink()
+        assert isinstance(filelink, Filelink)
+        assert filelink.handle == HANDLE
