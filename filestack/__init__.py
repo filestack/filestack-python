c44e3da72a9edb33ad190aee1fcefbfd49d541d1
Richard Herbert <richard.alan.herbert@gmail.com>
2017-05-26 13:36:18 -0700

    import audiovisual

--- filestack/__init__.py
+++ filestack/__init__.py
@@ -2,3 +2,4 @@ from .models.filestack_client import Client
 from .models.filestack_filelink import Filelink
 from .models.filestack_security import security
 from .models.filestack_transform import Transform
+from .models.filestack_audiovisual import AudioVisual
